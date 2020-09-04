29418f3112602c864a02f3257dc9eabb90c2cb45
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-04 17:32:58 -0400

    Widgets func component w/ proptypes setup

---
+++ src/components/Widgets/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Widgets () {
+  return (
+    <div className='widgets'>
+      Widgets
+    </div>
+  )
+}
+
+Widgets.propTypes = {
+
+}
