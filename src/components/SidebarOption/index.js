b9b2a2658465fa9fe0c757b752e8f08b609284f4
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-04 17:31:59 -0400

    SidebarOption component w/ proptypes setup

---
+++ src/components/SidebarOption/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function SidebarOption () {
+  return (
+    <div class='sidebar-option'>
+      Sidebar Option
+    </div>
+  )
+}
+
+SidebarOption.propTypes = {
+
+}
