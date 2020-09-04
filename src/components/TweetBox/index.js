b29b52184f9f6dd88e4fe18521ee358c3c909b48
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-04 17:32:34 -0400

    TweetBox func component w/ proptypes setup

---
+++ src/components/TweetBox/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function TweetBox () {
+  return (
+    <div className='tweet-box'>
+      Tweet Box
+    </div>
+  )
+}
+
+TweetBox.propTypes = {
+
+}
