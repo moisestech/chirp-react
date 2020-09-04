6b263b64c9e2944167f6d79135ac80c4ea469929
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-04 17:29:18 -0400

    Feed func component w/ proptypes setup

---
+++ src/components/Feed/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Feed () {
+  return (
+    <div className='feed'>
+      Feed
+    </div>
+  )
+}
+
+Feed.propTypes = {
+
+}
