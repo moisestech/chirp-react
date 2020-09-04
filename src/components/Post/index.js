175ff512cb4b40191ddb5e7270b7743447aa126d
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-04 17:29:58 -0400

    Post func component w/ proptypes setup

---
+++ src/components/Post/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Post () {
+  return (
+    <div className='post'>
+      Post
+    </div>
+  )
+}
+
+Post.propTypes = {
+
+}
