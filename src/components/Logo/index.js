5006c7b2f9fd885dd0b5bb279173d0dcd5217d05
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-09 07:42:12 -0400

    header logo png, slogan hear community

---
+++ src/components/Logo/index.js
@@ -0,0 +1,12 @@
+export default function Logo() {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="https://raw.githubusercontent.com/moisestech/chirp-react/master/public/Chirp_logo.png" />
+      </div>
+      <div className="slogan">
+        <h2>Hear your community</h2>
+      </div>
+    </div>
+  );
+}
