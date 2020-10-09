ad3ff58fb402cd84813c7249f3d2daccc5cdd442
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-09 07:30:38 -0400

    props active.bool text.string Icon.elementType, className sidebar-option --active

--- src/components/SidebarOption/index.js
+++ src/components/SidebarOption/index.js
@@ -1,14 +1,16 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-export default function SidebarOption () {
+export default function SidebarOption({ active, text, Icon }) {
   return (
-    <div class='sidebar-option'>
-      Sidebar Option
+    <div className={`sidebar-option ${active && "sidebar-option--active"}`}>
+      <Icon />
+      <h2>{text}</h2>
     </div>
-  )
+  );
 }
 
 SidebarOption.propTypes = {
-
-}
+  active: PropTypes.bool,
+  text: PropTypes.string.isRequired,
+  Icon: PropTypes.elementType.isRequired,
+};
