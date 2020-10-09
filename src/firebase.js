86bc260b7d2b57dc9d7bb6ad47869c536b1eb7db
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-09 07:31:47 -0400

    firebase cofig obj from chirp db fb app

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyC6OxgHoFFGvGJ-lorqUYyDgbx9XVjQ5s8",
+  authDomain: "chirp-2f4df.firebaseapp.com",
+  databaseURL: "https://chirp-2f4df.firebaseio.com",
+  projectId: "chirp-2f4df",
+  storageBucket: "chirp-2f4df.appspot.com",
+  messagingSenderId: "398596861956",
+  appId: "1:398596861956:web:7c9ab60719b9d2ec34c551",
+  measurementId: "G-90TCFXZHMP",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
