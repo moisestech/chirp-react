f6477e6ab1b5fddfa6effcfb8e9df758bd2a83e3
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-09 07:34:42 -0400

    imp ReactDOM, render doc html ID"root", imp .css

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// COMPONENTS
+import App from "./components";
+
+// REDUX
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+//const store = createStore(reducer, middleware);
 
 // Component
 // State
 // Lifecycle
 // UI
-
-function App() {
-  return (
-    <div>
-      <h1>Welcome to Chirp!</h1>
-      <img src="https://raw.githubusercontent.com/moisestech/chirp-react/master/public/Chirp_logo.png" />
-    </div>
-  );
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
