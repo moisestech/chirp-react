b3887f7c6243b24ea930b672be98c0c07e63d4d4
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-13 09:10:24 -0400

    updated Logo assets/ src

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <div className="header">
       <div className="logo">
-        <img src="https://raw.githubusercontent.com/moisestech/chirp-react/master/public/Chirp_logo.png" />
+        <img src="https://raw.githubusercontent.com/moisestech/chirp-react/master/public/assets/chirp_logo.png" />
       </div>
       <div className="slogan">
         <h2>Hear your community</h2>
