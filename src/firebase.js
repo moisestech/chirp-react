6349cb01c97472d1287acb75ed958a2115e27848
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-02 09:32:33 -0500

    init firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
-import firebase from "firebase";
+// import firebase from "firebase";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC6OxgHoFFGvGJ-lorqUYyDgbx9XVjQ5s8",
-  authDomain: "chirp-2f4df.firebaseapp.com",
-  databaseURL: "https://chirp-2f4df.firebaseio.com",
-  projectId: "chirp-2f4df",
-  storageBucket: "chirp-2f4df.appspot.com",
-  messagingSenderId: "398596861956",
-  appId: "1:398596861956:web:7c9ab60719b9d2ec34c551",
-  measurementId: "G-90TCFXZHMP",
-};
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC6OxgHoFFGvGJ-lorqUYyDgbx9XVjQ5s8",
+//   authDomain: "chirp-2f4df.firebaseapp.com",
+//   databaseURL: "https://chirp-2f4df.firebaseio.com",
+//   projectId: "chirp-2f4df",
+//   storageBucket: "chirp-2f4df.appspot.com",
+//   messagingSenderId: "398596861956",
+//   appId: "1:398596861956:web:7c9ab60719b9d2ec34c551",
+//   measurementId: "G-90TCFXZHMP",
+// };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const db = firebaseApp.firestore();
+// const db = firebaseApp.firestore();
 
-export default db;
+// export default db;
