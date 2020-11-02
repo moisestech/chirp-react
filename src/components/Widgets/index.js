83c790deb90a6a23f633936ae77f6001d69ccfce
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-02 09:31:26 -0500

    imp widgets.css, SearchIcon, Whats happening

--- src/components/Widgets/index.js
+++ src/components/Widgets/index.js
@@ -1,14 +1,37 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
+import "./widgets.css";
 
-export default function Widgets () {
+// import {
+//   TwitterTimelineEmbed,
+//   TwitterShareButton,
+//   TwitterTweetEmbed,
+// } from "react-twitter-embed";
+
+import SearchIcon from "@material-ui/icons/Search";
+
+export default function Widgets() {
   return (
-    <div className='widgets'>
-      Widgets
+    <div className="widgets">
+      <div className="widgets-input">
+        <SearchIcon className="widgets-search-icon" />
+        <input placeholder="Search Chirp" type="text" />
+      </div>
+
+      <div className="widgets-widget-container">
+        <h2>What's happening</h2>
+
+        {/* <TwitterTweetEmbed tweetId={"858551177860055040"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="moisesdsanabria"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"https://facebook.com/"}
+          options={{ text: "I study destiny #reactjs", via: "moisesdsanabria" }}
+        /> */}
+      </div>
     </div>
-  )
-}
-
-Widgets.propTypes = {
-
+  );
 }
