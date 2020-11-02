103e4b923bbef9cf360ecfb9066241bbd83fda98
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-02 09:32:59 -0500

    React.StrictMode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
 // COMPONENTS
-import App from "./components";
+import App from "./app";
 
 // REDUX
-import reducer from "./reducers";
-import middleware from "./middleware";
+import reducer from "./state/reducers";
+import middleware from "./state/middleware";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 
@@ -17,4 +18,10 @@ import { createStore } from "redux";
 // Lifecycle
 // UI
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+
+  document.getElementById("root")
+);
