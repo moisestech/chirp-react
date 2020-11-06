5f44a1d34522190395954e5917c8e318df591e86
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-06 18:25:53 -0500

    actionConstants string, actionCreator object to dispatch to Redux Reducers

---
+++ src/state/actions/authedUser.js
@@ -0,0 +1,10 @@
+// ACTION CONSTANT
+export const SET_AUTHED_USER = "SET_AUTHED_USER";
+
+// ACTION CREATOR
+export function setAuthedUser(id) {
+  return {
+    type: SET_AUTHED_USER,
+    id,
+  };
+}
