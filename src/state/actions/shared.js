56a55cfe3d4c55222ab32e3e1a1f25457a92b886
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-07 05:28:37 -0500

    getInitialData showLoading, hideLoading receiveUsers receiveChirps setAuthedUser

---
+++ src/state/actions/shared.js
@@ -0,0 +1,22 @@
+import { getInitialData } from "../../utils/api";
+import { setAuthedUser } from "../actions/authedUser";
+import { receiveUsers } from "../actions/users";
+import { receiveChirps } from "../actions/chirps";
+import { showLoading, hideLoading } from "react-redux-loading-bar";
+
+const AUTHED_ID = "moisessanabria";
+
+export function handleInitialData() {
+  return (dispatch) => {
+    dispatch(showLoading());
+
+    return getInitialData().then(({ users, tweets }) => {
+      const chirps = tweets;
+
+      dispatch(hideLoading());
+      dispatch(receiveUsers(users));
+      dispatch(receiveChirps(chirps));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
+  };
+}
